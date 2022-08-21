f8d835f5916e48050425e2855f49484c2b096d36
xao1215 <allexxpez@gmail.com>
2022-08-21 04:03:35 +0200

    typing func done

--- src/components/TypeRacer.js
+++ src/components/TypeRacer.js
@@ -23,9 +23,8 @@ const isSpace = (word) => {
 
 const TypeRacer = () => {
     const [words, setWords] = useState([])
-    const [done, setDone] = useState([])
     const [input, setInput] = useState("")
-    const which = useRef(0)
+    const which = useRef({word:0,char:0})
     const [current, setCurrent] = useState("")
 
     const focus = useRef(null)
@@ -37,59 +36,50 @@ const TypeRacer = () => {
     },[])
 
     const handleInput = (e) => {
-        console.log(e.target.value)
+        let newInput = e.target.value
 
         // if valid
-        setInput(e.target.value)
-        setDone(e.target.value)
+        setInput(newInput)
 
-        if(e.target.value.charAt( e.target.value.length-1 ) === current.charAt(0)){   //if right char
-            if( current.length === 1 ){    //if end of char
-                which.current = which.current + 1
-                setCurrent( words[which.current] )
-                setInput("")
-                // setDone( d => {
-                //     let n = structuredClone(d)
-                //     n.push("omg")
-                //     return n
-                // })
-            }else{    // if not end
-                setCurrent( c => c.slice(1)) 
-                // setWords( prev => {
-                //     let temp = structuredClone(prev)    
-                //     temp[0] = prev[0].slice(1)
-                //     return temp
-                // } )
+        let right = 0
+
+        for(let i = 0; i < newInput.length; i++){
+            if(! (newInput.charAt(i) === current.charAt(i)) ){
+                break;
             }
+            right += 1
         }
-        // setInput( e.target.value )
+        console.log("true : " + right)
+        if(current.length === right){
+            which.current.word += 1
+            which.current.char = 0
+            setCurrent( words[which.current.word] )
+            setInput("")
+        }else{
+            which.current.char = right
+        }
+
     }
-
-    const kek = (word) => {
-        let omg = [...word]
-        return ((word === " ") ? <>&nbsp;</> : word)
-    }
-
-
 
     return (
         <div className="flex h-full w-full items-center justify-center">
             {/*outline with gradient*/}
             <div className="bg-slate-400 h-5/6 w-5/6 items-center justify-center flex " >
-                <div  onClick={()=>{console.log(which.current);console.log(current);console.log(words)}} className="flex h-5/6 w-4/5 overflow-hidden bg-slate-100 text-black">
 
-                    <div className="bg-slate-300 text-3xl w-1/2 items-center flex justify-end">
+                <div className="flex h-5/6 w-4/5 overflow-hidden bg-slate-100 text-black">
+
+                    <div className="bg-slate-300 text-3xl w-1/2 leading-normal items-center flex justify-end">
                         <div className="absolute whitespace-nowrap">
-                            {words.slice(0,which.current).map( w => isSpace(w) ) } { [...input].map( w => isSpace(w))}
+                            {words.slice(0,which.current.word).map( w => isSpace(w) ) }
+                            { [...input].map( (w,i) => <div className={`inline-block ${ i < which.current.char ? "bg-green-500" : "bg-red-400"} text-transparent`}>{isSpace(w)}</div>)} 
                         </div>
-                        <input autoFocus={true} value={input} onChange={handleInput} spellCheck="false" className="text-right absolute whitespace-nowrap m-0 p-0 right-1/2 bg-transparent outline-none ">
-                            </input>
+                        <input autoFocus={true} value={input} onChange={handleInput} spellCheck="false" className="text-right caret-black absolute whitespace-nowrap m-0 right-1/2 bg-transparent outline-none ">
+                        </input>
                     </div>
-                    <div className="bg-slate-500 text-3xl w-1/2 items-center flex justify-start">
-                        {/* for overflow control set parent to relative */}
 
+                    <div className="bg-slate-300 text-3xl w-1/2 items-center leading-normal flex justify-start">
                         <div className=" w-1/2 overflow-hidden absolute  whitespace-nowrap">
-                            { isSpace(current) }{words.map( (w,i) => ( i > which.current ) ? (isSpace(w)) : "" )}
+                            { [...(current.slice(which.current.char,current.length))].map( char => isSpace(char)) }{words.map( (w,i) => ( i > which.current.word ) ? (isSpace(w)) : "" )}
                         </div>
                     </div>
                     
