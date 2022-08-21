90298ad897fc07d1ae443dfdd8d2a31904de478f
xao1215 <allexxpez@gmail.com>
2022-08-21 18:49:02 +0200

    styling, timer init

--- src/components/TypeRacer.js
+++ src/components/TypeRacer.js
@@ -27,8 +27,10 @@ const getRandomWords = () => {
     return result
 }
 
-const isSpace = (word) => {
-    return ((word === " ") ? <>&nbsp;</> : word)
+const isSpace = (word, t) => {
+    const classes = t ? "inline-block opacity-25" : "inline-block"
+    return ( <div className={classes}> {(word === " ") ? <>&nbsp;</> : word} </div> )
+
 }
 
 const TypeRacer = () => {
@@ -75,29 +77,38 @@ const TypeRacer = () => {
     // option to see stats once you start, reset button, try query random api, usecontext for navbar routes
 
     return (
-        <div className="flex h-full w-full items-center justify-center">
-            {/*outline with gradient*/}
-            <div onClick={ () => focusInput() } className="bg-slate-400 h-5/6 w-5/6 items-center justify-center flex " >
+        <div className="flex flex-col gap-10 h-full w-full items-center justify-center">
 
-                <div  className="flex h-5/6 w-4/5 overflow-hidden bg-slate-100 text-black">
+            <div onClick={ () => focusInput() } className="text-5xl tracking-wide font-thin text-white bg-custom-900 shadow-2xl h-36 w-full items-center justify-center flex " >
 
-                    <div className="bg-slate-300 text-3xl w-1/2 leading-normal items-center flex justify-end">
+                <div  className="flex overflow-hidden">
+
+                    <div className="w-1/2 leading-normal items-center flex justify-end">
                         <div className="absolute whitespace-nowrap">
-                            {words.slice(0,which.current.word).map( w => isSpace(w) ) }
-                            { [...input].map( (w,i) => <div className={`inline-block ${ i < which.current.char ? "bg-green-500" : "bg-red-400"} text-transparent`}>{isSpace(w)}</div>)} 
+                            {words.slice(0,which.current.word).map( w => isSpace(w,true) ) }
+                            { [...input].map( (w,i) => <div className={`inline-block ${ i < which.current.char ? "bg-green-500" : "bg-red-400"} text-transparent`}>{isSpace(w,false)}</div>)} 
                         </div>
-                        <input ref={focus} autoFocus={true} value={input} onChange={handleInput} spellCheck="false" className="text-right caret-black absolute whitespace-nowrap m-0 right-1/2 bg-transparent outline-none ">
+                        <input ref={focus} autoFocus={true} value={input} onChange={handleInput} spellCheck="false" className="text-right absolute whitespace-nowrap m-0 right-1/2 bg-transparent outline-none ">
                         </input>
                     </div>
 
-                    <div className="bg-slate-300 text-3xl w-1/2 items-center leading-normal flex justify-start">
-                        <div className=" w-1/2 overflow-hidden absolute  whitespace-nowrap">
-                            { [...(current.slice(which.current.char,current.length))].map( char => isSpace(char)) }{words.map( (w,i) => ( i > which.current.word ) ? (isSpace(w)) : "" )}
+                    <div className="w-1/2 items-center leading-normal flex justify-start">
+                        <div className=" w-1/2 overflow-hidden absolute whitespace-nowrap">
+                            { [...(current.slice(which.current.char,current.length))].map( char => isSpace(char,false)) }
+                            {words.map( (w,i) => ( i > which.current.word ) ? (isSpace(w,true)) : "" )}
                         </div>
                     </div>
+                    {/* <div className="absolute w-96 h-1/2 bg-red-500"></div> */}
                     
                 </div>
+
+
+
             </div>
+            <div className="pt-12 pb-14 w-full align-middle text-5xl whitespace-nowrap tracking-wide font-thin bg-custom-900 text-white items-center justify-center overflow-hidden flex">
+                wpm cpm time-left restart     
+            </div>
+
         </div>
     )
 }
